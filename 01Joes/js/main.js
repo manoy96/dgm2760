2f5ad7df4a4844e99b9f1ad055b5a4f054663f24
Unknown <mfewcar@gmail.com>
2017-10-03 17:36:13 -0600

    Module 1 finished

---
+++ 01Joes/js/main.js
@@ -0,0 +1,33 @@
+// JavaScript Document
+/*jslint browser:true */
+"use strict";
+
+
+var header = document.getElementById("header");
+
+//JS adding HEADER
+var joe = "Joe's Bed & Breakfast";
+var h_one = document.createElement("h1");
+h_one.innerHTML = joe;
+header.appendChild(h_one);
+
+var slogan = "where dreams come to life";
+var h_two = document.createElement("h2");
+h_two.innerHTML = slogan;
+header.appendChild(h_two);
+
+// var slogan = "where dreams come to life";
+// document.getElementById("slogan").innerHTML = slogan;
+// var h_two = document.createElement("h2");
+
+
+// JS insert current date
+var myDateString = new Date();
+document.getElementById("date").innerHTML = myDateString.toDateString();
+
+
+//JS prompt user's name
+var username = prompt("What is your name");
+
+var mycup = "hello "+username+" welcome to a lovely place to call home";
+document.getElementById("message").innerHTML = mycup;
